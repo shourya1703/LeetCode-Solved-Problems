d69ced4926c715bd7bd2d8b4285ada1b4b941146
shourya1703 <75567286+shourya1703@users.noreply.github.com>
2021-04-17 14:26:02 +0530

    Time: 8 ms (18.39%), Space: 35.6 MB (58.21%) - LeetHub

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        int ans=1;
+        if(n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+        
+            
+        }
+    }
