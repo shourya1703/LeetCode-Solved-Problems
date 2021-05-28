9ea2a9f5e9a99b3dda6abf0053d32b1f04b09a94
shourya1703 <shouryanamdeo17@gmail.com>
2021-05-29 00:06:12 +0530

    Time: 444 ms (5.17%), Space: 39.5 MB (29.00%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -1,15 +1,32 @@
+/*class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int temp=nums[0];
+        for(int i=0;i<nums.length;i++)
+             if(nums[i]==temp){
+                 temp++;
+             }
+             else{
+                return temp;
+             }
+        return 1;
+    }
+}*/
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum=0;
-       
-        for(int i=0;i<nums.length;i++){
-            sum=sum+nums[i];
-        }
-            int total=0;
+        int found =0;
         for(int i=0;i<=nums.length;i++){
-           total=total+i;
-        }   
-        
-            return total-sum;
+            found=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]==i){
+                    found = 1;
+                    
+                }
+            }
+            if(found!=1){
+                return i;
+            }
+        }
+        return -1;
     }
 }
