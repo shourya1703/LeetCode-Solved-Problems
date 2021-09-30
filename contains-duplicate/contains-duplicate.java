cf93d1016751d33ac1464a964527254c3b415562
shourya1703 <shouryanamdeo17@gmail.com>
2021-09-30 08:41:48 +0530

    Time: 6 ms (67.12%), Space: 46.7 MB (39.84%) - LeetHub

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,13 +1,12 @@
+import java.util.HashSet;
+import java.util.ArrayList;
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-                
-            }
-               }
-        return false;
-        }
-        
-    }
+
+public boolean containsDuplicate(int[] nums) {
+Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+if(nums.length == set.size())
+    return false;
+else
+    return true;
+}
+}
